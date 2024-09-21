4fce0af8c574e60519e8ac90ba54d637ef5535a7
om shankar <om8003golu@gmail.com>
2024-09-21 08:10:03 +0530

    Custom Tabs -showing selected tab with border - used state logic

--- src/components/CustomTabs.jsx
+++ src/components/CustomTabs.jsx
@@ -1,8 +1,15 @@
 // problem statement :
 // make multiple tabs to navigate on multiple pages.
 
-import React from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+  useLocation,
+} from "react-router-dom";
 
 const CustomTabs = () => {
   return (
@@ -20,21 +27,39 @@ const CustomTabs = () => {
 export default CustomTabs;
 
 const Homewrapper = () => {
+  const { pathname } = useLocation();
+  const [selectedPath, setSelectedPath] = useState(pathname);
   const navigate = useNavigate();
   const handleRoute = (path) => {
     navigate(path);
+    setSelectedPath(path);
   };
   return (
     <div>
       <h1>Custom Tabs</h1>
       <ul className="flex m-2 space-x-4">
-        <li onClick={() => handleRoute("/")} className="cursor-pointer ">
+        <li
+          onClick={() => handleRoute("/")}
+          className={`cursor-pointer ${
+            selectedPath === "/" ? "border-b-2 border-red-600" : ""
+          } `}
+        >
           Home
         </li>
-        <li onClick={() => handleRoute("/about")} className="cursor-pointer">
+        <li
+          onClick={() => handleRoute("/about")}
+          className={`cursor-pointer ${
+            selectedPath === "/about" ? "border-b-2 border-red-600" : ""
+          } `}
+        >
           About
         </li>
-        <li onClick={() => handleRoute("/contact")} className="cursor-pointer">
+        <li
+          onClick={() => handleRoute("/contact")}
+          className={`cursor-pointer ${
+            selectedPath === "/contact" ? "border-b-2 border-red-600" : ""
+          } `}
+        >
           Contact
         </li>
       </ul>
